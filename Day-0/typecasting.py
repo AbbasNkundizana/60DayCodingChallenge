a726d4cc3184b94d79c991484f36bee6fb6dca67
AbbasNkundizana <your.abbasnkundizana@gmail.com>
2023-11-04 22:50:05 +0300

    typecasting

---
+++ Day-0/typecasting.py
@@ -0,0 +1,21 @@
+"""""
+name = 'Nkundizana'
+print(len(name))
+
+length = len('Nkundizana')
+print('Your name has'  +str(length)+  'characters')
+
+print(type(length))
+new_length = str(length)
+print(type(new_length))
+"""
+
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+
+sum = first_number + second_number
+print(sum)
+
+
+
+
