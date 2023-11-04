c9f5b102db0a6e6b602d1566aa4f33f0b6fb0ea6
AbbasNkundizana <your.abbasnkundizana@gmail.com>
2023-11-04 22:04:33 +0300

    datatypes

---
+++ Day-0/datatypes.py
@@ -0,0 +1,51 @@
+#var_1 = 0b10
+name = 'Tomi'
+age = 25
+score = 97.5
+sex = True
+print(type(name))
+print(type(age))
+print(type(score))
+print(type(sex))
+
+#to skip meaning of quotes use back slash
+#print "I am not aware of that", He said.
+#var_1 = 0b10
+name = 'Tomi'
+age = 25
+score = 97.5
+sex = True
+print(type(name))
+print(type(age))
+print(type(score))
+print(type(sex))
+
+#to skip meaning of quotes use back slash
+#print "I am not aware of that", He said.
+print('\"I am not aware of that\", \\n He said.')
+#var_1 = 0b10
+name = 'Tomi'
+age = 25
+score = 97.5
+sex = True
+print(type(name))
+print(type(age))
+print(type(score))
+print(type(sex))
+
+#to skip meaning of quotes use back slash
+#print "I am not aware of that", He said.
+print('\"I am not aware of that\", \\n He said.')
+#var_1 = 0b10
+name = 'Tomi'
+age = 25
+score = 97.5
+sex = True
+print(type(name))
+print(type(age))
+print(type(score))
+print(type(sex))
+
+#to skip meaning of quotes use back slash
+#print "I am not aware of that", He said.
+print('\"I am not aware of that\", \\n He said.')
