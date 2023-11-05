042db6f2a5e78cfa4c9b7dba152b096cf6ccca07
AbbasNkundizana <your.abbasnkundizana@gmail.com>
2023-11-05 10:28:19 +0300

    operators

---
+++ Day-1/operators.py
@@ -0,0 +1,16 @@
+a = 80
+b = 40
+
+print(a//b)
+
+print(5+(2*3)-1+(10/5))
+
+
+Weight = int(input("Enter your weight in kilograms: "))
+Height = float(input("Enter weight in meters: "))
+
+BMI = Weight/(Height **2)
+print(f"Your BMI is {BMI}")
+
+
+
