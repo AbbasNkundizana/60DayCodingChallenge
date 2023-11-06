b3330179795383ba595aacb62e150e20e17fe831
AbbasNkundizana <your.abbasnkundizana@gmail.com>
2023-11-06 23:04:56 +0300

    for loops

---
+++ Day-2/loops.py
@@ -0,0 +1,16 @@
+# for i in range(5):
+#     print("Faiza Abdullah Bawazir")
+
+# names = ['Faiza', 'Mona', 'Hana', 'Neemah']
+
+# for name in names:
+#     print(name)
+
+# cities = ['Mbarara', 'Khartoum', 'Seoul', 'Glasgow']
+# for city in cities:
+#     print(city)
+
+countries = ['Uganda', 'Sudan', 'Korea', 'Scotland']
+for country in countries:
+    print(country)
+
