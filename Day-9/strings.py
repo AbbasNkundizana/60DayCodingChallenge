c1b8cc61049a23da670351ca721f3c88a591690e
AbbasNkundizana <your.abbasnkundizana@gmail.com>
2023-11-14 23:03:56 +0300

    strings

---
+++ Day-9/strings.py
@@ -0,0 +1,8 @@
+print(len('NkundizanaAbbasIsma'))
+print(len('FaizaAbdullahBawazir'))
+print(len('MunaAbdallahBawazir'))
+print(len('64grndrgefjerehjheyyty5uyuy3i4uytuyg4ythhrhtyghrhtjgrythrheiuythfhu4huiuou2i34u'))
+print(len('4'))
+print('Nkundizana'+' '+'Abbas'+' '+'Isma')
+print('Faiza' *5)
+
