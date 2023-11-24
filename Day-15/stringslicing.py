5467acd60ddf7613f44984395603b8487ae5bc0b
AbbasNkundizana <your.abbasnkundizana@gmail.com>
2023-11-24 13:57:03 +0300

    stringslicing with indexing

---
+++ Day-15/stringslicing.py
@@ -0,0 +1,11 @@
+name = 'Nkundizana Abbas Isma'
+print(name[11:16])
+print(name[0:10])
+print(name[17:])
+print(name[:])
+
+# name[0:5] extracts characters from index 0 to 4 (inclusive).
+# name[7:12] extracts characters from index 7 to 11 (inclusive).
+# name[0:10:2] extracts characters from index 0 to 9, skipping every second character.
+# name[:5] extracts characters from the beginning up to index 4.
+# name[7:] extracts characters from index 7 to the end of the string.
