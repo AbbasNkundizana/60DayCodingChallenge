d58b2a5f9b88337d721bc709ad6aba3800162609
AbbasNkundizana <your.abbasnkundizana@gmail.com>
2023-11-24 18:25:35 +0300

    special characters

---
+++ Day-19/specialcharacters.py
@@ -0,0 +1,25 @@
+#\n for new line
+print("This will\nprint on\na new line")
+#\t for tab
+print("How old was the Aisha when the Prophet PBUH married her?\n\t12")
+#\"
+print("The rain outsode is insanely alot, \"Said the supervisor\"")
+
+#use backslash to print a special character
+
+
+#use raw character "r" to print some special characters
+file_name = r"C:\AbbasNkunda\60DayCodingChallenge\Day19"
+print(file_name)
+
+#Breaking expressions into multiple lines without losing means
+some_random_number = 30
+some_other_random_number_collected_from_a_certain_book = 46
+some_other_multiple_number_collected_and_corrected_from_a_certain_book_within_the_library = 97
+
+sum = (
+some_random_number
++ some_other_random_number_collected_from_a_certain_book +\
+    + some_other_multiple_number_collected_and_corrected_from_a_certain_book_within_the_library
+)
+
