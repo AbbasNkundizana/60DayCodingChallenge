8be0e7be9f41779d7feb3f916887a4cdb5770a4a
AbbasNkundizana <your.abbasnkundizana@gmail.com>
2023-11-26 22:53:50 +0300

    lists

---
+++ Day-20/lists.py
@@ -0,0 +1,14 @@
+empty = []
+print(empty)
+
+friends = ["Anh", "Bridget", "Caberie", "Fiona", "Peace", "Eve", "Muna"]
+print(len(friends))
+scores = [95, 60, 70, 99, 89, 45]
+print(len(scores))
+heights = [1.98, 1.56, 1.23]
+print(len(heights))
+payments = [1000,2000,2300,4000,54000,2332000,]
+print(len(payments))
+bools = [True, False, False, True, False]
+
+
