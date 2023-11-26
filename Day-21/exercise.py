d8d533ff75e8a09f3a74853e40d354bcd6ba3ac0
AbbasNkundizana <your.abbasnkundizana@gmail.com>
2023-11-26 23:44:52 +0300

    lists exercise

---
+++ Day-21/exercise.py
@@ -0,0 +1,18 @@
+#Create an empty list and assign it to the variable "empty"
+empty = []
+
+#Create a list with a single Boolean -True and assign it to the variable "active"
+active = [True]
+
+
+# Create a list with 5 integers of your choice and assign it to the variable "favorite_numbers"
+favorite_numbers = [5, 7, 8, 30,99]
+
+# Create a list with three strings "red", "green", "blue" and assign it to the variable "colours"
+colours = ["red", "green", "blue", ]
+
+# Declare an is_long function that accepts a single list as an argument
+# it should return True if the list has more than 5 elements, and False otherwise
+
+def is_long(elements):
+    return len(ements) > 5
