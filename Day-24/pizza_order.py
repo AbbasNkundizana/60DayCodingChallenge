28a50ad2032515b1745ce234c5588e5d26dfca79
AbbasNkundizana <your.abbasnkundizana@gmail.com>
2023-11-29 22:50:26 +0300

    pizza order

--- Day-24/pizza_order.py
+++ Day-24/pizza_order.py
@@ -11,13 +11,13 @@ pizza_size = input("What pizza size would you like to order? S/M/L?: ")
 
 if pizza_size == 'S' or pizza_size == 's':
     bill = 100
-    print(f"Your bill is {bill}Rs")
+    print(f"Small pizza bill is {bill}Rs")
 elif pizza_size == 'M' or pizza_size == 'm':
     bill = 200
-    print(f"Your bill is {bill} Rs")
+    print(f"Medium pizza bill is {bill} Rs")
 else:
     bill = 300
-    print(f"Your bill is {bill}Rs")
+    print(f"Large pizza bill is {bill}Rs")
     
 
 pepperoni = input("Would you like pepperoni with your pizza?: Y/N: ")
@@ -25,22 +25,23 @@ pepperoni = input("Would you like pepperoni with your pizza?: Y/N: ")
 if pepperoni == 'Y' or pepperoni == 'y':
     if pizza_size == 'S' or pizza_size == 's':
         bill += 30 
-        print(f"Your bill is {bill}")
+        print(f"Your pizza bill is {bill}")
     elif pizza_size == 'M' or pizza_size == 'm':
         bill += 50 
-        print(f"Your bill is {bill}")
+        print(f"Your pizza bill is {bill}")
     else:
         bill += 50 
-        print(f"Your bill is {bill}")
+        print(f"Your pizza bill is {bill}")
 else:
-    print(f"Your bill is {bill}")
+    print(f"Your pizza bill is {bill}")
 
 extra_cheese = input("Do you need extra cheese on your pizza?: Y/N: ")
 if extra_cheese == 'Y' or extra_cheese == 'y':
     bill += 20
-    print(f"Your bill is {bill}")
+    print(f"Your pizza bill is {bill}")
 else:
-    print(f"Your bill is {bill}")
+    print(f"Your final pizza bill is {bill}")
+
 
 
     
